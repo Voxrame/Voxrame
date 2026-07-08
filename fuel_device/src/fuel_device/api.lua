df25a7f729672178c6c59f5dd14f988cca9278ba
lord-papirus <cm.603038@gmail.com>
2026-07-08 23:58:39 +0300

    Added sound functions to fuel_device api

--- fuel_device/src/fuel_device/api.lua
+++ fuel_device/src/fuel_device/api.lua
@@ -1,10 +1,12 @@
 local Device    = require('fuel_device.Device')
+local Sound     = require('fuel_device.Sound')
 local Processor = require('fuel_device.Processor')
 local node      = require('fuel_device.node')
 
 
 return {
 	Device    = Device,
+	Sound     = Sound,
 	Processor = Processor,
 	register  = node.register,
 }
