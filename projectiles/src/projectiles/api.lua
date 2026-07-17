e75f36784dc2f9f9704715d0aa38ec4665a51328
Living Broscillograph <totservis@gmail.com>
2026-07-17 21:06:11 +0300

    Update projectiles/src/projectiles/api.lua
    linter warning: trailing whitespace

--- projectiles/src/projectiles/api.lua
+++ projectiles/src/projectiles/api.lua
@@ -98,7 +98,7 @@ local explode_objects = function(pos, radius, explosive_object, damage_groups)
 end
 -- Invoke on_blast() callback in the node
 --- @param pos    table       position of the node
-local explode_node = function(pos)	
+local explode_node = function(pos)
 	local node = core.get_node(pos)
 	local node_def = core.registered_nodes[node.name]
 	
