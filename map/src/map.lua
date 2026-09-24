aeb05a24e08f68812fe35d92ab0502b498a46d40
Alexander Chibrikin <alek13.me@gmail.com>
2026-09-24 09:35:12 +0300

    helpers, map: declare `Voxrame` as `@class (partial)` so EmmyLua merges its fields across mods. VX-24.

--- map/src/map.lua
+++ map/src/map.lua
@@ -1,21 +1,25 @@
 
+--- @class (partial) Voxrame
 Voxrame = Voxrame or {}
 
---- @module 'Voxrame.map'
+--- @class Voxrame.map
 Voxrame.map = {
 	--- @type Voxrame.map.Room
 	Room = require('map.Room'),
 	--- @type Voxrame.map.Corridor
 	Corridor = require('map.Corridor'),
-	--- @namespace Voxrame.map.room
+	--- @class Voxrame.map.room
 	room = {
 		--- @type Voxrame.map.room.Wall
 		Wall  = require('map.room.Wall'),
-		--- @namespace Voxrame.map.room.wall
+		--- @class Voxrame.map.room.wall
 		wall  = {
+			--- @type Voxrame.map.room.wall.Type
 			Type = require('map.room.wall.Type')
 		},
+		--- @type Voxrame.map.room.Walls
 		Walls = require('map.room.Walls'),
+		--- @type Voxrame.map.room.Exit
 		Exit  = require('map.room.Exit'),
 	},
 }
